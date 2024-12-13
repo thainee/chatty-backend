82fc8812df15d1270fcbfda06e5d48f63d2fc1d2
thainee <thaivohong2003@gmail.com>
2024-12-13 22:36:12 +0700

    feat (auth): log out

---
+++ src/features/auth/controllers/logout.ts
@@ -0,0 +1,9 @@
+import HTTP_STATUS from 'http-status-codes';
+import { Request, Response } from 'express';
+
+export class LogOut {
+  public async logOut(req: Request, res: Response): Promise<void> {
+    req.session = null;
+    res.status(HTTP_STATUS.OK).json({ message: 'Logged out successfully' });
+  }
+}
--- src/features/auth/routes/auth.router.ts
+++ src/features/auth/routes/auth.router.ts
@@ -1,4 +1,5 @@
 import { LogIn } from '@auth/controllers/login';
+import { LogOut } from '@auth/controllers/logout';
 import { SignUp } from '@auth/controllers/signup';
 import express, { Router } from 'express';
 
@@ -7,15 +8,18 @@ export class AuthRouter {
 
   constructor() {
     this.router = express.Router();
-    this.initRoutes();
-  }
-
-  private initRoutes(): void {
-    this.router.post('/signup', SignUp.prototype.signUp);
-    this.router.post('/login', LogIn.prototype.logIn);
   }
 
   public routes(): Router {
+    this.router.post('/signup', SignUp.prototype.signUp);
+    this.router.post('/login', LogIn.prototype.logIn);
+
+    return this.router;
+  }
+
+  public logoutRoute(): Router {
+    this.router.get('/logout', LogOut.prototype.logOut);
+
     return this.router;
   }
 }
